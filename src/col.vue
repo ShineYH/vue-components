8833379a135aab1750d90f79438ec3126758bb66
孙宇航 <sunyuhang@hellobike.com>
2019-08-14 19:18:09 +0800

    栅格布局

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,8 @@
 <template>
-  <div :class="['g-col', `col-${span}`]">
-    <slot></slot>
+  <div :class="['g-col', span && `col-${span}`, offset && `offset-${offset}`]" :style="colGutterClass">
+    <div style="border: 1px solid grey;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,7 +12,19 @@ export default {
   props: {
     span: {
       type: [ Number, String ],
-      default: 24
+    },
+    offset: {
+      type: [ Number, String ],
+    }
+  },
+  data() {
+    return {
+      gutter: 0
+    }
+  },
+  computed: {
+    colGutterClass() {
+      return `padding: 0 ${this.gutter / 2}px;`
     }
   }
 }
@@ -18,11 +32,14 @@ export default {
 <style lang='scss'>
 .g-col {
   height: auto;
-  border: 1px solid grey;
+  padding: 0 10px;
 
   @for $n from 1 through 24 {
     &.col-#{$n} {
       width: ($n / 24) * 100%;
+    }
+    &.offset-#{$n} {
+      margin-left: ($n / 24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="g-row">
+  <div class="g-row" :style="rowGutterClass">
     <slot></slot>
   </div>
 </template>
@@ -7,6 +7,22 @@
 <script>
 export default {
   name: 'gRow',
+  props: {
+    gutter: {
+      type: [Number, String],
+      default: 0
+    }
+  },
+  computed: {
+    rowGutterClass() {
+      return `margin: 0 ${-this.gutter / 2}px`;
+    }
+  },
+  mounted() {
+    this.$children.forEach(child => {
+      child.gutter = this.gutter;
+    });
+  }
 }
 </script>
 <style lang='scss'>
